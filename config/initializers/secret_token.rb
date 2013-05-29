287a6ee62a15d70c904f6d7d878a0cccbce11ccf
Bob Cavezza <rcavezza@gmail.com>
2013-05-28 22:05:33 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Innovoxppc::Application.config.secret_token = '97e5031466cf72fbdc33be6832ba4b15cbda353f42fe3e38fdd1085ae6467697c2c244958a397646a6c7f49032d9985692e65ef740e162106a27f7e27482c197'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Innovoxppc::Application.config.session_store :cookie_store, key: '_innovoxppc_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Innovoxppc::Application.config.session_store :active_record_store
