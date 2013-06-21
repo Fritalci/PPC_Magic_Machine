ca9a267024ea6d96ee3e599ad2969baa534c3be4
Bob Cavezza <rcavezza@gmail.com>
2013-06-20 21:42:25 -0400

    Adds create xls adwors spreadsheet button into active admin products index page

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,3 +1,16 @@
 ActiveAdmin.register Product do
+
+  index do                            
+    column :title
+    column :url 
+    column :isbn 
+    column 'Create Adwords Spreadsheet' do |product|
+    	link_to('Create Spreadsheet',product_path(id: product.id, format:'xls'))
+    end
+		column :created_at
+		column :updated_at
+    default_actions
+  end                                 
+
   
 end
